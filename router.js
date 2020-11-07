083137f7394578aec9e736018c39b67a39722c8a
nao-mix <69722862+nao-mix@users.noreply.github.com>
2020-11-07 15:59:54 +0100

    Update router.js

--- router.js
+++ router.js
@@ -6,7 +6,9 @@ import Home from './components/Home.vue';
 import Cafe from './components/Cafe';
 import Spiel from './components/Spiel';
 import MyApps from './components/MyApps';
+import Organist from './components/Organist';
 import Ueber from './components/Ueber';
+import Contact from './components/Contact';
 
 // Vue Router
 Vue.use(VueRouter);
@@ -31,10 +33,25 @@ const router = new VueRouter({
             component: MyApps
         },
         {
+            path: '/organist', 
+            component: Organist
+        },
+        {
             path: '/ueber', 
             component: Ueber
-      }
-    ]
+        },
+        {
+            path: '/kontakt', 
+            component: Contact
+        }
+    ],
+    scrollBehavior (to, from, savedPosition) {
+        if (savedPosition) {
+           return savedPosition
+        } else {
+           return { x: 0, y: 0 }
+        }
+    }
 }); 
 
 export default router;
