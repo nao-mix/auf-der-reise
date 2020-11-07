091c4b6a3c438d91be4c93e4759fb0f21f5b75f9
NaomiK <naomi.kurata@web.de>
2020-11-07 15:50:49 +0100

    change main.js

--- main.js
+++ main.js
@@ -2,15 +2,29 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router.js';
 
+// axios
+import axios from 'axios';
+Vue.prototype.$axios = axios;
+
 //Bootstrap
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 Vue.use(BootstrapVue);
 
+// reCaptcha v3
+import { VueReCaptcha } from 'vue-recaptcha-v3';
+Vue.use(VueReCaptcha, {
+    siteKey: 'MY_SITEKEY',
+    loaderOptions: {
+        useRecaptchaNet: true,
+        autoHideBadge: true
+    }
+}); 
+
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
